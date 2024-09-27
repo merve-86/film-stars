d9fd062ce1c7f562e3c9ef8a541287e2536bfb31
merve <merveaydin86@hotmail.com>
2024-09-27 21:10:48 +0300

    up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-      <h1 className='title'>MOVIE STARS</h1>
+      <h1 className="title">MOVIE STARS</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
